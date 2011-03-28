b16e9fae331b02f7fe97276ac7ec468f4031bd9b
Bradford <bradford@measuringmeasures.com>
2011-03-27 17:30:11 -0700

    all tests passing using new monolithic vertex composition.'

--- src/work/graph.clj
+++ src/work/graph.clj
@@ -7,52 +7,6 @@
    [work.queue :as workq])
   (:use    [plumbing.core ]))
 
-(defprotocol Inbox
-  (receive-message [this msg])
-  (poll-message [this]))
-
-(defprotocol Outbox
-  (broadcast [this x]))
-
-(defn drain-to-vertex
-  [vertex xs]
-  (doseq [x xs] (receive-message (:inbox vertex) x))
-  vertex)
-
-(defrecord  InboxQueue [q]
-  Inbox
-  (receive-message [this msg] (workq/offer q msg))
-  (poll-message [this] (workq/poll q)))
-
-(defrecord  DefaultOutbox [children]
-  Outbox
-  (broadcast [this x]
-	     (doseq [{:keys [when,to]} children
-		     :when (when x)]
-	       (to x))))
-
-(defn run-vertex
-  "launch vertex return vertex with :pool field"
-  [{:keys [f,inbox,outbox,threads,sleep-time,exec,multimap]
-    :or {threads (work/available-processors)
-	 sleep-time 10
-	 exec work/sync
-	 multimap list}
-    :as vertex}]
-  (let [work-producer
-	(work/work (fn []
-		     {:f f
-		      :in #(poll-message inbox)
-		      :out (fn [x]
-			     (doall (map #(broadcast outbox %)
-					 (multimap x))))
-		      :exec exec})
-		   (work/sleeper sleep-time))]
-    (assoc vertex
-      :pool (future (work/queue-work
-		     work-producer
-		     threads)))))
-
 (defn node
   [f  &
    {:keys [id]
@@ -62,6 +16,9 @@
       (merge opts)
       (assoc :id id)))
 
+(defn multimap [f & args]
+  (apply node f :multimap true args))
+
 (defn root-node []
   (node identity :id :root))
 
@@ -69,9 +26,9 @@
   [root]
   (zip/zipper
    ;; branch?
-   (fn [x] (not (:terminal x)))
+   :children
    ;; children
-   #(:children %)
+   :children
    ;; make-node
    (fn [x cs] (assoc-in x [:children] cs))
    ; root 
@@ -90,13 +47,34 @@
   (-> parent-loc
       (zip/edit add-child child)
       zip/down
-      zip/rightmost
-      zip/down))
+      zip/rightmost))
 
 (defn- all-vertices [root]
   (for [loc  (zf/descendants (graph-zip root))
 	:when (and loc (->> loc zip/node))]
     (zip/node loc)))
+
+(defn run-vertex
+  "launch vertex return vertex with :pool field"
+  [{:keys [f,in,out,threads,sleep-time,exec,multimap]
+    :or {threads (work/available-processors)
+	 sleep-time 10
+	 exec work/sync
+	 multimap list}
+    :as vertex}]
+  (let [work-producer
+	(work/work (fn []
+		     {:f f
+		      :in #(workq/poll in)
+		      :out (fn [x]
+			     (doall (map #(workq/offer out %)
+					 (multimap x))))
+		      :exec exec})
+		   (work/sleeper sleep-time))]
+    (assoc vertex
+      :pool (future (work/queue-work
+		     work-producer
+		     threads)))))
 
 (defn run-graph
   "launches in DFS order the vertex processs and returns a map from
@@ -110,13 +88,19 @@
 	(zip/root loc)
 	(recur (-> loc update zip/next))))))
 
-(defn graph-comp [{:keys [f children]}]
-  (if (not children) f
-      (fn [& args]
-	(let [fx (apply f args)]
-	  (doall (map (fn [child]
-			((graph-comp child) fx))
-		      children))))))
+(defn graph-comp [{:keys [f children multimap when]}]
+  (if (not children)
+    (if (not when) f
+	(fn [& args]
+	  (if (apply when args)
+	    (apply f args))))
+    (fn [& args]
+      (let [fx (apply f args)]
+	(doall (map (fn [child]
+		      (if (not multimap)
+			((graph-comp child) fx)
+			(doall (map (graph-comp child) fx))))
+		    children))))))
 
 (defn out [f q]
   (fn [& args]
--- test/work/graph_test.clj
+++ test/work/graph_test.clj
@@ -10,10 +10,7 @@
   "Test helper fn waits until the pool finishes processing before returning results."
   [response-q expected-seq-size]
   (wait-until #(= (.size response-q) expected-seq-size) 5)
-  (sort (iterator-seq (.iterator response-q))))
-
-(defn queue-seq [q]
-  (sort (iterator-seq (.iterator q))))
+  (iterator-seq (.iterator response-q)))
 
 (deftest graph-comp-test
   (let [inc-graph
@@ -26,92 +23,42 @@
 	idq (q/local-queue)
 	root (-> (root-node)
 		 graph-zip
-		 (child (node (out inc incq)
-			      :id :inc))
-		 (child (node (out identity idq)
-			      :id :identity )))]
+		 (child (node (out inc incq)))
+		 (child (node (out identity idq))))]
     (mono-run root (range 5))
     (is (= (range 1 6) (wait-for-complete-results incq 5)))
     (is (= (range 5) (wait-for-complete-results idq 5)))))
 
-#_(deftest one-node-graph-test
-  (let [root (-> (root-node)
-		 (drain-to-vertex (range 5))
+(deftest multimap-graph-test
+  (let [multiq (q/local-queue)
+	root (-> (root-node)
 		 graph-zip
-		 (child (node :f inc :id :inc))
-		 (child (node :id :identity))
-		 run-graph)
-	out (terminal-queues root)]
-    (is (= (range 1 6) (wait-for-complete-results (:inc out) 5)))
-    (is (= (range 5) (wait-for-complete-results (:identity out) 5)))
-    (kill-graph root)))
+		 (child-> (multimap range))
+		 (child (node (out inc multiq))))]
+    (mono-run root (range 4))
+    (is (= [1 1 2 1 2 3]
+	     (wait-for-complete-results multiq 5)))))
 
-;; (deftest multimap-graph-test
-;;   (let [root (-> (root-node)
-;; 		 (drain-to-vertex (range 4))
-;; 		 graph-zip
-;; 		 (add-edge (node :f identity
-;; 					  :id :multimap
-;; 					  :multimap (fn [x] (range x))))
-;; 		 run-graph)
-;; 	out (terminal-queues root)]
-;;     (is (= [0 0 0 1 1 2] (wait-for-complete-results (:multimap out) 5)))
-;;     (kill-graph root)))
+(deftest chain-graph-test
+  (let [incq (q/local-queue)
+	plus2q (q/local-queue)
+	root (-> (root-node)
+		 graph-zip
+		 (child-> (node inc))
+		 (child (node (out inc incq)))
+		 (child (node (out (partial + 2)
+				   plus2q))))]
+    (mono-run root (range 5))
+    (is (= (range 2 7) (wait-for-complete-results incq 5)))
+    (is (= (range 3 8) (wait-for-complete-results plus2q 5)))))
 
-;; (deftest chain-graph-test
-;;   ; (range 5) -> inc -> inc
-;;   (let [root (-> (root-node)
-;; 		 (drain-to-vertex (range 5))
-;; 		 graph-zip
-;; 		 (add-edge-> (node inc))
-;; 		 (add-edge (node :f inc :id :out))
-;; 		 (add-edge (node :f (partial + 2) :id :plus-two))
-;; 		 run-graph)
-;; 	outs (terminal-queues root)]
-;;     (is (= (range 2 7) (wait-for-complete-results (:out outs) 5)))
-;;     (is (= (range 3 8) (wait-for-complete-results (:plus-two outs) 5)))
-;;     (kill-graph root)))
-
-;; (deftest graph-test
-;;   (let [input-data (range 1 101 1)
-;;         root (-> (root-node)
-;; 		 (drain-to-vertex input-data)
-;; 		 graph-zip
-;; 		 (add-edge-> (node (partial * 10) :threads 2))
-;; 		 (add-edge (node :f inc :id :output))
-;; 		 run-graph)
-;; 	out (terminal-queues root)]
-;;     (is (= (map (fn [x] (inc (* 10 x))) (range 1 101 1))
-;; 	   (wait-for-complete-results (:output out) (count input-data))))
-;;     (kill-graph root)))
-
-;; (deftest simple-dispatch-test
-;;   (let [root (-> (root-node)
-;; 		 (drain-to-vertex [1 2 3 4 5])
-;; 		 graph-zip
-;; 		 (add-edge-> (node identity))
-;; 		 (add-edge (node :id :even)
-;; 			   :when even?)
-;; 		 (add-edge (node :id :odd)
-;; 			   :when odd?)
-;; 		 (add-edge (node :id :all))
-;; 		 (add-edge (node :f inc :id :plus-1)
-;; 			   :when odd?)
-;; 		 run-graph)
-;; 	outs (terminal-queues root)]
-;;     (Thread/sleep 200)
-;;     (kill-graph root)
-;;     (is (= (-> outs :even seq sort) [2 4]))
-;;     (is (= (-> outs :odd seq sort) [1 3 5]))
-;;     (is (= (-> outs :all seq sort) (range 1 6)))
-;;     (is (= (-> outs :plus-1 seq sort) (map inc [1 3 5])))))
-
-;; (deftest simple-sideffect-test
-;;   (let [a (atom nil)
-;; 	root (-> (root-node)
-;; 		 (drain-to-vertex [1 2 3 4 5])
-;; 		 graph-zip
-;; 		 (add-edge-> (fn [x] (swap! a conj x)))
-;; 		 run-graph)]
-;;     (Thread/sleep 2000)
-;;     (is (= (sort @a) [1 2 3 4 5]))))
+(deftest simple-predicate-test
+  (let [incq (q/local-queue)
+	idq (q/local-queue)
+	root (-> (root-node)
+		 graph-zip
+		 (child (node (out identity idq) :when even?))
+		 (child (node (out inc incq) :when odd?)))]
+    (mono-run root (range 5))
+    (is (= (range 2 5 2) (wait-for-complete-results incq 5)))
+    (is (= (range 0 5 2) (wait-for-complete-results idq 5)))))
