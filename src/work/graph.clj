a6afab706731a674587bc88f09c3243d103d8fc8
Bradford <bradford@measuringmeasures.com>
2011-04-17 23:05:51 -0700

    Revert "proper worker size for graphs in a pool."
    This reverts commit 60b16c33d3f8a5ee0cb063132f096a070ce130aa.

--- src/work/graph.clj
+++ src/work/graph.clj
@@ -106,8 +106,7 @@
     :as vertex}]
   (assoc vertex
     :pool (let [pool (Executors/newFixedThreadPool (int threads))]
-	    (dotimes [_ threads]
-	      (work/submit-to pool (constantly vertex)))
+	    (work/submit-to pool (constantly vertex))
 	    pool)))
 
 (defn run-graph
