00f2081f11faa2187c2f75a0f766f5348020fa63
Bradford <bradford@measuringmeasures.com>
2011-05-09 21:48:54 -0700

    updating deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-sys/plumbing "0.1.5-SNAPSHOT"]
-                 [store "0.2.3-SNAPSHOT"]]
+                 [store "0.2.4-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :repositories {"snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
                  "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
