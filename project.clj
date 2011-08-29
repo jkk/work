ebc4c0f24d971b0575a81cda66909841adce2698
Bradford Cross <bradford@measuringmeasures.com>
2011-08-29 14:09:57 -0700

    Bump store version #

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-sys/plumbing "0.1.5-SNAPSHOT"]
-                 [store "0.2.4-SNAPSHOT"]
+                 [store "0.2.5-SNAPSHOT"]
 		 [services "0.0.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
